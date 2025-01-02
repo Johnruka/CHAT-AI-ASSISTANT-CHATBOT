fb7e2c1bb47841f1ee9e4c514850b37d8dd4e6f6
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2025-01-02 15:17:07 +0100

    Initial commit

---
+++ src/main/java/org/example/chataiassistantchatbot/ChatAiAssistantChatbotApplication.java
@@ -0,0 +1,13 @@
+package org.example.chataiassistantchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAiAssistantChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAiAssistantChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/chataiassistantchatbot/ChatAiAssistantChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chataiassistantchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAiAssistantChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
